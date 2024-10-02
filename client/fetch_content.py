f2433195c62e0b619897812599b6864ff9095575
Jesse Borden <jesse.j.borden@gmail.com>
2024-10-02 19:52:26 -0400

    add endpoints, reuse code

--- client/fetch_content.py
+++ client/fetch_content.py
@@ -1,13 +1,16 @@
 import requests
 
-def fetch_content():
+def fetch_path(route):
     try:
         # Fetch content from the Flask web server running on container1
-        response = requests.get('http://server:5000')
+        response = requests.get(f'http://server:5000{route}')
         response.raise_for_status()  # Raise an error for bad status codes
         print(response.text)  # Print the content of the response
     except requests.exceptions.RequestException as e:
         print(f"Error fetching content: {e}")
 
 if __name__ == "__main__":
-    fetch_content()
+    fetch_path("/")
+    fetch_path("/skills")
+    fetch_path("/weaknesses")
+    fetch_path("/finalthought")
--- server/app.py
+++ server/app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello():
     return "Greetings from the Flask Web Server, client request was successul!"
 
+@app.route('/skills')
+def skill():
+    return "I am very skilled in DevSecOps, and Prisma Cloud!"
+
+@app.route('/weaknesses')
+def weakness():
+    return "I have no weaknesses that I can't overcome!"
+
+@app.route('/finalthought')
+def finalthought():
+    return "I admit my flaws, and admit this app is not the best, but we basically just made our own fly by night API! Woot woot!\nStarting cleanup now!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
